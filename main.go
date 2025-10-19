d42cd77cbc4e1c048303ddd4405500015be36e55
isaackleiner <idenfyy@gmail.com>
2025-10-19 20:24:19 +0300

    delete coin system

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math/rand"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -21,19 +20,6 @@ const (
 	barsWindowHeight = 5
 )
 
-// создание монеток
-func setupCoins(level int) []*Sprite {
-	coins := make([]*Sprite, level+2)
-	for index := 0; index < level+2; index++ {
-		coins[index] = NewSprite(
-			'0',
-			rand.Intn(gameWidth-2)+1,
-			rand.Intn(gameHeight-2)+1,
-		)
-	}
-	return coins
-}
-
 func main() {
 	screen, err := tcell.NewScreen()
 	if err != nil {
@@ -46,7 +32,6 @@ func main() {
 	// game init section
 	player := NewSprite('@', gameWidth/2, gameHeight/2)
 	player.Color = tcell.Color(tcell.ColorGold)
-	coins := setupCoins(1)
 
 	score := 0
 	level := 1
@@ -68,11 +53,7 @@ func main() {
 
 		screen.Clear()
 		drawFrame(screen)
-
 		player.Draw(screen)
-		for _, coin := range coins {
-			coin.Draw(screen)
-		}
 
 		// ui
 		// drawString(screen, 1, 1, fmt.Sprintf("Score: %d", score))
@@ -111,7 +92,6 @@ func main() {
 		screen.Show()
 
 		// Управление
-		playerMoved := false
 		ev := screen.PollEvent()
 		// проверили тип события
 		switch ev := ev.(type) {
@@ -124,42 +104,17 @@ func main() {
 				if player.Y > 1 {
 					player.Y--
 				}
-				playerMoved = true
 			case 'a':
 				if player.X > 1 {
 					player.X--
-					playerMoved = true
 				}
 			case 's':
 				if player.Y < gameHeight-2 {
 					player.Y++
-					playerMoved = true
 				}
 			case 'd':
 				if player.X < gameWidth-2 {
 					player.X++
-					playerMoved = true
-				}
-			}
-		}
-		// check coin collision
-		if playerMoved {
-			coinCollectedIndex := -1
-			for index, coin := range coins {
-				if coin.X == player.X && coin.Y == player.Y {
-					// collect coin
-					coinCollectedIndex = index
-					score++
-				}
-			}
-			// handle coin collision
-			if coinCollectedIndex > -1 {
-				coins[coinCollectedIndex] = coins[len(coins)-1]
-				coins = coins[0 : len(coins)-1]
-
-				if len(coins) == 0 {
-					level++
-					coins = setupCoins(level)
 				}
 			}
 		}
